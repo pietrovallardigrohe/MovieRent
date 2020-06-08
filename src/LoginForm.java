0ad7fc52b1b0e3b78bc530eb8a6a1080e6116ba6
Pietro Vallardi Grohe <55392971+krontorn@users.noreply.github.com>
2020-06-08 20:05:58 -0300

    Login Page added

---
+++ src/LoginForm.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class LoginForm extends JFrame {
+
+    private JPanel root;
+    private JTextField loginField;
+    private JLabel loginLabel;
+    private JLabel passwordLabel;
+    private JButton signinButton;
+    private JPasswordField passwordField;
+
+    public LoginForm() {
+
+        setContentPane(root);
+        setSize(400,300);
+        setVisible(true);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocation(Toolkit.getDefaultToolkit().getScreenSize().width/2-this.getSize().width/2,Toolkit.getDefaultToolkit().getScreenSize().height/2-this.getSize().height/2 );
+
+
+
+    }
+
+}
